886925af41bfc90ee9e38c5590929e5fab4904f1
Pvalleci <45024304+Pvalleci@users.noreply.github.com>
2018-11-29 17:22:48 +0100

    Add files via upload

--- get_next_line/get_next_line2.c
+++ get_next_line/get_next_line2.c
@@ -1,99 +1,203 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line2.c                                   :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pvalleci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: juaalvar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/23 13:19:42 by pvalleci          #+#    #+#             */
-/*   Updated: 2018/11/23 17:14:32 by pvalleci         ###   ########.fr       */
+/*   Created: 2018/11/23 21:16:22 by juaalvar          #+#    #+#             */
+/*   Updated: 2018/11/28 13:57:39 by pvalleci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#define BUFF_SIZE 32
 
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+#include "get_next_line.h"
 
-int	ft_check_line(char *s)
+size_t	ft_strlen(const char *s)
 {
-	int i;
+	size_t i;
 
 	i = 0;
-	while (s[i] != '\n' || s[i])
+	while (s[i])
 		i++;
-	if (s[i] == '\n')
-		return (i);
-	return (0);
+	return (i);
 }
 
-void	ft_clean_static(char **s, int n)
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	int i;
-	char	*tmp;
+	size_t	i;
 
-	while (*s[i])
-		i++;
-	tmp = malloc(i);
 	i = 0;
-	while(*s[i])
+	while (i < n && src[i])
 	{
-		tmp[i] = *s[i];
+		dest[i] = (char)src[i];
 		i++;
 	}
-	tmp[i] = 0;
-	*s = malloc(i);
-	ft_memcpy(*s, tmp, i);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
 
-int	get_next_line(int fd, char **line)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	static char *s = NULL;
+	int		i;
+	char	*d;
+	char	*s;
+
+	i = 0;
+	d = (char *)dest;
+	s = (char *)src;
+	while (n > 0)
+	{
+		d[i] = s[i];
+		i++;
+		n--;
+	}
+	return ((void*)dest);
+}
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*newlist;
+
+	if (!(newlist = (t_list*)malloc(sizeof(t_list) * 1)))
+		return (0);
+	newlist->content = ((void*)malloc(ft_strlen(content)));
+	if (newlist->content == NULL)
+		return (0);
+	ft_memcpy(newlist->content, content, ft_strlen(content));
+	newlist->content_size = content_size;
+	newlist->next = NULL;
+	return (newlist);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		size;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!(s1 && s2))
+		return (0);
+	size = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(str) * (size + 1))))
+		return (NULL);
+	while (s1[i])
+		str[j++] = (char)s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = (char)s2[i++];
+	str[j] = '\0';
+	return (str);
+}
+
+char	*ft_getfile(const int fd)
+{
+	char *s = NULL;
 	int nblu;
-	int n;
-	char	buf[BUFF_SIZE + 1];
+	char buf[BUFF_SIZE + 1];
 
 	nblu = 0;
-		if (!s)
-		{
-			s = malloc(BUFF_SIZE + 1);
-			nblu = read(fd, s, BUFF_SIZE);
-			printf("nblu: %d\n", nblu);
-			s[nblu] = 0;	
-			printf("%s\n", s);
-			printf("OK");
-		}
-			n = ft_check_line(s);
-			printf("%d\n", n);
-			if (n > 0)//il y a un '\n'
-			{
-				ft_memcpy(*line, s, n);
-				ft_clean_static(&s+n , n);
-				return (1);
-			}
-			else//il n  y a pas de \n
-			{
-			//	realloc la s
-				;
-				//strjoin de s et buf
-			}
+	if (!s)
+	{
+		nblu = read(fd, buf, BUFF_SIZE);
+		buf[nblu] = '\0';
+		if (!(s = (char *)malloc(nblu)))
+			return (NULL);
+		s = ft_strncpy(s, buf, nblu);
+	}
+	while ((nblu = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[nblu] = '\0';
+		s = ft_strjoin(s, buf);
+	}
+	printf("file : %s\n", s);
+	return (s);
+}
+
+void	ft_affiche_liste(t_list *maillon)
+{
+	printf("maillon->content_size : %d\n", (int)maillon->content_size);
+	if (maillon->next != NULL)
+	{
+		ft_affiche_liste(maillon->next);
+	}
+}
+
+char	*ft_clean_content(char *s, int i)
+{
+	char *tmp;
+	int j;
+
+	j = 0;
+	i++;
+	while (s[i + j] != '\0')
+		j++;
+	tmp = (char *)malloc(j);
+	tmp = ft_strncpy(tmp, s + i, j);
+	free(s);
+	s = NULL;
+	return (tmp);
+}
+
+t_list	*ft_parcour_list(t_list *maillon, const int fd)
+{
+	if (maillon->content_size != (size_t)fd && maillon->next != NULL)
+		return (ft_parcour_list(maillon->next, fd));
+	else if (maillon->content_size != (size_t)fd && maillon->next == NULL)
+	{
+		printf("LE MAILLON N'A PAS ETE TROUVE\nCREATION D UN NOUVEAU MAILLON\n");
+		maillon->next = ft_lstnew(ft_getfile(fd), fd);
+		return (maillon->next);
+	}
+	printf("LE MAILLON EXISTE\n");
+	return (maillon);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	printf("\n--------------------------------\n\n");
+	static t_list *maillon = NULL;
+	t_list *ptr;
+	int		i;
+
+	i = 0;
+	if (!maillon)
+	{
+		maillon = ft_lstnew(ft_getfile(fd), fd);
+		ptr = maillon;		
+	}
+	else	
+		ptr = ft_parcour_list(maillon, fd);
+	while (ptr->content[i] != '\n' && ptr->content[i])
+		i++;
+	*line = (char *)malloc(ft_strlen(ptr->content));
+	*line = ft_strncpy(*line, ptr->content, i);
+	printf("line : %s\n", *line);
+	ptr->content = ft_clean_content(ptr->content, i);
+	printf("reste : %s", ptr->content);
+	printf("\n--------------FIN--------------\n\n");
 	return (0);
 }
 
-
-int	main(void)
+int		main(void)
 {
-	int	fd;
+	int fd1;
+	int fd2;
 	char *line;
 
-	fd = open("test", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	close(fd);
-	return (0);
+	fd1 = open("texte1", O_RDONLY);
+	fd2 = open("texte2", O_RDONLY);
+	get_next_line(fd1, &line);
+	get_next_line(fd2, &line);
+	get_next_line(fd1, &line);
+	close(fd1);
+	close(fd2);
+	return 0;
 }
+
