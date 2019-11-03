85bcb8a4c222e543423047d44a34ca656666ef89
paul V <paulvallecillo@gmail.com>
2019-11-03 21:57:15 +0100

    Valid

---
+++ ft_printf/+/src/ft_aff_resultat.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_aff_resultat.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvalleci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 06:53:40 by pvalleci          #+#    #+#             */
+/*   Updated: 2019/10/29 06:53:43 by pvalleci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static char		*ft_input_str(char *format, t_param *list, char *str)
+{
+	int i;
+	int j;
+	int l;
+
+	i = 0;
+	l = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (list->result[j])
+			{
+				str[l] = list->result[j];
+				l++;
+				j++;
+			}
+			i = i + ft_skip_param(format + i);
+			list = list->next;
+		}
+		else
+			str[l++] = format[i];
+		i++;
+	}
+	return (str);
+}
+
+int				ft_affiche_resultat(char *format, t_param *list)
+{
+	int		len;
+	int		i;
+	char	*str;
+	t_param	*start;
+
+	len = 0;
+	i = -1;
+	start = list;
+	while (format[++i])
+	{
+		if (format[i] == '%')
+		{
+			len = len + ft_strlen(list->result);
+			list = list->next;
+			i = i + ft_skip_param(format + i);
+			len--;
+		}
+		len++;
+	}
+	if (!(str = ft_strnew(len)))
+		return (0);
+	ft_input_str(format, start, str);
+	write(1, str, len);
+	free(str);
+	return (len);
+}
